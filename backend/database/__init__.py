fa307b52a5617eb6c54683ddf3700bae43a9144d
Jared Healy <healy_jared@yahoo.com>
2026-05-26 11:57:34 +0800

    db

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -1,6 +1,7 @@
 import os
+from datetime import datetime
 
-from pony.orm import Database, PrimaryKey, Required, Optional, Set
+from pony.orm import Database, PrimaryKey, Required, Optional, Set, composite_key
 
 from config import get_config
 
@@ -13,7 +14,9 @@ def init_db():
     db.generate_mapping(create_tables=False)
 
 
-NullableOptional = lambda a: Optional(a, nullable=True)
+def NullableOptional(a, *args, **kwargs):
+    return Optional(a, nullable=True, *args, **kwargs)
+
 
 class Umpires(db.Entity):
     id = PrimaryKey(int, auto=True)
@@ -24,30 +27,72 @@ class Umpires(db.Entity):
     games_second_official = Set('Games', reverse='official_two')
 
 
+class Clubs(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    code = Required(str, unique=True)
+    long_name = Required(str)
+    image_link = NullableOptional(str)
+    time_created = NullableOptional(int)
+    home_games = Set('Games', reverse='home_team')
+    away_games = Set('Games', reverse='away_team')
+
+
+class Competitions(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    level = Required(str)
+    gender = Required(str)
+    age_level = Required(str)
+    is_premier = Required(bool)
+    year = Required(int)
+    altius_id = NullableOptional(int, unique=True)
+    time_created = NullableOptional(int)
+    games = Set('Games', reverse='competition')
+    composite_key(gender, level, year)
+
+class Venues(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    code = Required(str)
+    long_name = Required(str)
+    turf_number = Required(int)
+    time_created = NullableOptional(int)
+    games = Set('Games', reverse='venue')
+    composite_key(code, turf_number)
+
 class Games(db.Entity):
     id = PrimaryKey(int, auto=True)
-    blob = Required(str)
-    team_one = Required(str)
-    team_two = Required(str)
-    team_one_long_name = Required(str)
-    team_two_long_name = Required(str)
-    team_one_image = Required(str)
-    team_two_image = Required(str)
-    competition_name = Required(str)
+    home_team = Required(Clubs, column='home_team_id')
+    away_team = Required(Clubs, column='away_team_id')
     start_time = Required(int)
-    last_server_ping = Required(int)
-    time_created = NullableOptional(int)
-    is_live = Required(bool)
-    altius_link = NullableOptional(str)
-    teamstar_link = NullableOptional(str)
-    official_one = Optional(Umpires)
-    official_two = Optional(Umpires)
-    clips = Set('Clips')
+    competition = Required(Competitions, column='competition_id')
+    official_one = NullableOptional(Umpires, column='official_one_id')
+    official_two = NullableOptional(Umpires, column='official_two_id')
+    altius_id = NullableOptional(str, unique=True)
+    teamstar_id = NullableOptional(str, unique=True)
+    live_hockey_id = NullableOptional(str, unique=True)
+    stream_start_time = NullableOptional(int)
+    home_team_score = NullableOptional(int)
+    away_team_score = NullableOptional(int)
+    venue = NullableOptional(Venues, column='venue_id')
+    clips = Set('Clips', reverse='game')
+
+    def format_for_frontend(self):
+        game = self.to_dict()
+        if self.official_one:
+            game['officials'] = [self.official_one.name]
+            del game['official_one']
+        if self.official_two:
+            game['officials'].append(self.official_two.name)
+            del game['official_two']
+        game['competition'] = self.competition.to_dict()
+        game['start_time'] *= 1000
+        if game.get('stream_start_time', None):
+            game['stream_start_time'] *= 1000
+        return game
 
 
 class Clips(db.Entity):
     id = PrimaryKey(int, auto=True)
-    game_id = Required(Games)
+    game = Required(Games, column="game_id")
     name = Required(str)
     start_time = Required(int)
     duration = Required(int)
@@ -57,16 +102,9 @@ class Clips(db.Entity):
     time_created = NullableOptional(int)
 
 
-class Images(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    name = Required(str)
-    link = Required(str)
-    time_created = NullableOptional(int)
-
-
 class Users(db.Entity):
     id = PrimaryKey(int, auto=True)
     username = Required(str)
-    live_hockey_token = Optional(str, nullable=True)
-    whistle_iq_token = Optional(str, nullable=True)
-    whistle_iq_session = Optional(str, nullable=True)
+    live_hockey_token = NullableOptional(str)
+    whistle_iq_token = NullableOptional(str)
+    whistle_iq_session = NullableOptional(str)
