56c53aa33b4823001c38703de0c7e14fddd1fa1b
healy <healy_jared@yahoo.com>
2026-06-04 13:06:16 +0800

    fix the project so it will build

--- frontend/src/app/appointments/[name]/page.tsx
+++
@@ -1,9 +0,0 @@
-import {IndividualGraph} from "@/components/IndividualGraph";
-
-export default async function Page({params}: {
-    params: Promise<{ name: string }>
-}) {
-    const {name} = await params
-
-    return <IndividualGraph name={name}/>
-}
